5a1c3f38735c2c10512cee6b2b425cfd2549d4fb
ADyBaH <Brandon4@mail.ru>
2022-12-20 22:19:08 +0300

    feat: add solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nString = n.toString().split("").reverse();
+    if (n.toString().startsWith("-")) nString.pop();
+    return Number(nString.join(""));
+};
